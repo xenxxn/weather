4ab1cb20993317711f98b0607707b3098ee28492
zenxxn <bunbun2438@naver.com>
2023-08-28 20:49:01 +0900

    08/28 2차 실습과제 chapter.04 진행중

---
+++ src/test/java/zerobase/weather/WeatherApplicationTests.java
@@ -0,0 +1,16 @@
+package zerobase.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@SpringBootTest
+class WeatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/zerobase/weather/repository/JdbcMemoRepositoryTest.java
@@ -0,0 +1,44 @@
+package zerobase.weather.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.util.AssertionErrors;
+import org.springframework.transaction.annotation.Transactional;
+import zerobase.weather.domain.Memo;
+import zerobase.weather.repository.JdbcMemoRepository;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+@Transactional
+public class JdbcMemoRepositoryTest {
+    @Autowired
+    JdbcMemoRepository jdbcMemoRepository;
+
+    @Test
+    void insertMemoTest(){
+        //given
+        Memo newMemo = new Memo(2, "insert Memo Test");
+        //when
+        jdbcMemoRepository.save(newMemo);
+        //then
+        Optional<Memo> result = jdbcMemoRepository.findById(2);
+        assertEquals(result.get().getText(), "insert Memo Test");
+    }
+
+    @Test
+    void findAllMemoTest(){
+        //given
+        List<Memo> memoList = jdbcMemoRepository.findAll();
+        //when
+        System.out.println(memoList);
+        //then
+        assertNotNull(memoList);
+    }
+}
